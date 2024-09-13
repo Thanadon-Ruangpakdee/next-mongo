32157aed969c41bb3def49a953a5d13235ab918b
Thanadon_Ruangpakdee <u6610308@au.edu>
2024-09-13 15:21:29 +0700

    Update

--- app/api/category/[id]/route.js
+++
@@ -1,7 +0,0 @@
-import Category from "@/models/Category";
-
-export async function GET(request, { params }) {
-    const id = params.id;
-    const category = await Category.findById(id)
-    return Response.json(category);
-}
